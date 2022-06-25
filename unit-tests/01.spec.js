02e64abf2ccff4ec1b2aad08d1293d1809467bc1
Farren Casad <fcasad@gmail.com>
2022-06-24 20:43:41 -0700

    Set up unit testing with jest

--- unit-tests/01.spec.js
+++ unit-tests/01.spec.js
@@ -2,6 +2,13 @@
 01.
 log to the console the count of all the people
 */
-test('does something', () => {
-	expect(true).toBe(true);
-})
+test("logs the correct count", () => {
+  // Arrange
+  jest.spyOn(global.console, "log").mockImplementation(() => {});
+  // Act
+  require("../challenges/01.js");
+  // Assert
+  expect(console.log).toBeCalledWith(6);
+  // Cleanup
+  console.log.mockRestore();
+});
