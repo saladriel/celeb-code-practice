d585462404ed1031dcd138ec0e5f0f49e6862a89
DESKTOP-SFEKABV\Coral Miller <coral1608@gmail.com>
2022-07-09 20:36:27 -0700

    completed challenges 15 & 16.

--- challenges/15.js
+++ challenges/15.js
@@ -3,3 +3,14 @@
 write a function 'averageAge' that given a list of people returns their average age (rounded down 
 to the nearest integer)
 */
+const age = require("./09");
+
+module.exports = function averageAge(people) {
+  let totalAgeCombined = 0;
+  let index = 0;
+  while (index < people.length) {
+    totalAgeCombined = totalAgeCombined + age(people[index]);
+    index++;
+  }
+  return Math.floor(totalAgeCombined / people.length);
+};
--- challenges/16.js
+++ challenges/16.js
@@ -2,3 +2,15 @@
 16.
 write a function 'fullNames' that given a list of people returns a list of just their full names
 */
+const fullName = require("./07");
+
+module.exports = function fullNames(people) {
+  let index = 0;
+  const names = [];
+  while (index < people.length) {
+    const name = fullName(people[index]);
+    names.push(name);
+    index++;
+  }
+  return names;
+};
