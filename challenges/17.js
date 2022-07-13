29d86ffdd9a9ad8993d0a17e589f8f120f26c1bd
DESKTOP-SFEKABV\Coral Miller <coral1608@gmail.com>
2022-07-12 18:29:08 -0700

    challenges 17 & 18

--- challenges/17.js
+++ challenges/17.js
@@ -3,3 +3,18 @@
 write a function 'fullNamesOfOverAchievers' that given a list of people returns a list of full
 names of only the people who over-achieved with their partner
 */
+const fullName = require("./07");
+const overAchievedWithPartner = require("./14");
+
+module.exports = function fullNamesOfOverAchievers(people) {
+  let index = 0;
+  const overAchievers = [];
+  while (index < people.length) {
+    if (overAchievedWithPartner(people[index])) {
+      const name = fullName(people[index]);
+      overAchievers.push(name);
+    }
+    index++;
+  }
+  return overAchievers;
+};
--- challenges/18.js
+++ challenges/18.js
@@ -3,3 +3,19 @@
 write a function 'peopleSummaries' that given a list of people returns a list of people summaries
 (full name and age)
 */
+const fullName = require("./07");
+const age = require("./09");
+
+module.exports = function peopleSummaries(people) {
+  let index = 0;
+  const summaries = [];
+  while (index < people.length) {
+    const summary = {
+      name: fullName(people[index]),
+      age: age(people[index]),
+    };
+    summaries.push(summary);
+    index++;
+  }
+  return summaries;
+};
