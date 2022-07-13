11650d318112befaa0a1dcce92266254081b5b6c
DESKTOP-SFEKABV\Coral Miller <coral1608@gmail.com>
2022-07-12 20:33:15 -0700

    commiting 19

--- challenges/19.js
+++ challenges/19.js
@@ -3,3 +3,19 @@
 write a function 'fullNamesByAttractivenessTier' that given a list of people returns an object
 where each key is a tier and each value is an array of the full names of people in that tier
 */
+const attractivenessTier = require("./13");
+const fullName = require("./07");
+
+module.exports = function fullNamesByAttractivenessTier(people) {
+  const byAttractivenessTier = {};
+  let index = 0;
+  while (index < people.length) {
+    const tier = attractivenessTier(people[index]);
+    if (byAttractivenessTier[tier] === undefined) {
+      byAttractivenessTier[tier] = [];
+    }
+    byAttractivenessTier[tier].push(fullName(people[index]));
+    index++;
+  }
+  return byAttractivenessTier;
+};
